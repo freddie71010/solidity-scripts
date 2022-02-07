c60207bba74ad8a0a9f8c5294533dab6f74e3ded
Freddie von Stange <freddie71010@gmail.com>
2022-02-07 17:46:31 -0500

    Remove commented out 'pragma solidity vx.x.x' as it was causing compile issues - this is an issue with Brownie!

--- smartcontract-lottery/contracts/Lottery.sol
+++ smartcontract-lottery/contracts/Lottery.sol
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-// pragma solidity ^0.6.6;
-// import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-
 pragma solidity ^0.7.0;
 
 import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
