6e06ec313ec00914f1ddd7e11cee2eab825a770c
Freddie von Stange <freddie71010@gmail.com>
2022-05-26 13:26:38 -0400

    Update brownie-config, deploy script, and RandomNFT.sol

--- nft-ipfs/contracts/RandomNFT.sol
+++ nft-ipfs/contracts/RandomNFT.sol
@@ -1,35 +1,57 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink-brownie-contracts/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink-brownie-contracts/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
-    VRFCoordinatorV2Interface immutable i_vrfCoordinator;
-    bytes immutable i_gasLane;
-    bytes immutable i_subscriptionId;
-    uint32 immutable i_callbackGasLimit;
+error RangeOutOfBounds();
+error AlreadyInitialized();
 
-    uint16 constant REQUEST_CONFIRMATIONS = 3;
-    uint32 constant NUM_WORDS = 2;
-    uint32 constant MAX_CHANCE_VALUE = 99;
+contract DoggieWalkNFTs is ERC721URIStorage, VRFConsumerBaseV2 {
+    using Counters for Counters.Counter;
+    
+    // Dog Types
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+    
+    // Chainlink VRF Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 2;
 
+    // NFT Variables
+    Counters.Counter public tokenIdCounter;
+    uint256 constant MAX_CHANCE_VALUE = 100;
+    string[3] internal s_dogTokenURIs;
+    bool private s_initialized;
+
+    // VRF Helpers
     mapping(uint256 => address) s_requestIdToSender;
-
+    
+    // Events
+    event NftRequested(uint256 indexed requestId, address requester);
+    event NftMinted(Breed breed, address minter);
+    
     constructor(
-        address vrfCoordinatorV2,
-        bytes32 gasLane,
-        bytes32 s_subscriptionId,
-        uint16 s_requestConfirmations,
-        uint32 s_callbackGasLimit,
-        uint32 numWords
-    ) ERC721("Doggie Walk", "DW") {
-        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
-        i_gasLane = gasLane;
-        i_subscriptionId = s_subscriptionId;
-        REQUEST_CONFIRMATIONS = s_requestConfirmations;
-        i_callbackGasLimit = s_callbackGasLimit;
-        NUM_WORDS = numWords;
+        address _vrfCoordinatorV2,
+        bytes32 _gasLane,
+        uint32 _subscriptionId,
+        uint32 _callbackGasLimit,
+        string[3] memory dogTokenURIs
+    ) ERC721("Doggie Walk", "DW") VRFConsumerBaseV2(_vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorV2);
+        i_gasLane = _gasLane;
+        i_subscriptionId = _subscriptionId;
+        i_callbackGasLimit = _callbackGasLimit;
+        _initializeContract(dogTokenURIs);
     }
 
     function requestDoggie() public returns (uint256 requestId) {
@@ -40,25 +62,62 @@ contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
             i_callbackGasLimit, // max gas amount
             NUM_WORDS
         );
+        s_requestIdToSender[requestId] = msg.sender;
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
-        internal
-        override
-    {
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
         // owner of the dog
         address dogOwner = s_requestIdToSender[requestId];
         // assign the NFT a tokenId
-        uint256 newTokenId = s_tokenCounter;
-        s_tokenCounter = s_tokenCounter + 1;
-
+        uint256 newTokenId = tokenIdCounter.current();
+        tokenIdCounter.increment();
+        uint256 moddedRng = randomWords[0] % 100;
+        Breed breed = getBreedFromModdedRng(moddedRng);
         _safeMint(dogOwner, newTokenId);
+        // set the tokenURI of Dog
+        _setTokenURI(newTokenId, s_dogTokenURIs[uint256(breed)]);
+        emit NftMinted(breed, dogOwner);
+
     }
 
-    function getChanceArray() public pure returns (uint256[3] memory) {}
+    function getChanceArray() public pure returns (uint256[3] memory) {
+        // 0 - 9 = st bernard
+        // 10 - 59 = pug
+        // 30 - 99 = shiba inu
+        return [10, 30, MAX_CHANCE_VALUE];
+    }
 
-    function getBreedFromModdedRng(uint256 moddedRng) {
+    function getBreedFromModdedRng(uint256 moddedRng) public pure returns(Breed) {
         uint256 cumulativeSum = 0;
         uint256[3] memory chanceArray = getChanceArray();
+        for (uint256 i=0; i < chanceArray.length; i++) {
+            if (moddedRng >= cumulativeSum && moddedRng < cumulativeSum + chanceArray[i]) {
+                // 0 = st bernard
+                // 1 = pug
+                // 2 = shiba inu
+                return Breed(i);
+            }
+            cumulativeSum = cumulativeSum + chanceArray[i];
+        }
+        revert RangeOutOfBounds();
     }
+
+    function _initializeContract(string[3] memory dogTokenUris) private {
+        if (s_initialized) {
+            revert AlreadyInitialized();
+        }
+        s_dogTokenURIs = dogTokenUris;
+        s_initialized = true;
+    }
+
+    // getter functions
+    function getDogTokenUris(uint256 index) public view returns (string memory) {
+        return s_dogTokenURIs[index];
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return tokenIdCounter.current();
+    }
+
+
 }
--- nft-ipfs/scripts/deploy.py
+++ nft-ipfs/scripts/deploy.py
@@ -1,55 +1,20 @@
 from tracemalloc import start
-from brownie import Lottery, accounts, config, network
+from brownie import DoggieWalkNFTs, accounts, config, network
 from .utils import get_account, LOCAL_BLOCKCHAIN_ENVS, get_contract, fund_with_link, print_line
 import time
 
 
-def deploy_lottery():
+def deploy_doggies():
     account = get_account()
-    lottery = Lottery.deploy(
-        get_contract("eth_usd_price_feed").address,
-        get_contract("vrfcoordinator").address,
-        get_contract("link_token").address,
+    doggieWalkNFTs = DoggieWalkNFTs.deploy(
+        config["networks"][network.show_active()]["vrfcoordinator_v1"],
         config["networks"][network.show_active()]["fee"],
-        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["keyhash_v1"],
         {"from": account},
-        publish_source=config["networks"][network.show_active()].get("verify", False),
+        publish_source=config["networks"][network.show_active()].get("verify", True),
     )
-    print_line("Deployed Lottery!")
-    return lottery
-
-def start_lottery():
-    account = get_account()
-    lottery = Lottery[-1]
-    starting_tx = lottery.startLottery({"from": account})
-    starting_tx.wait(1)
-    print_line("Lottery has started!")
-
-def enter_lottery():
-    account = get_account()
-    lottery = Lottery[-1]
-    value = lottery.getEntranceFee() + 100000000
-    tx = lottery.enter({"from": account, "value": value})
-    tx.wait(1)
-    print_line("You entered the lottery!")
-
-def end_lottery():
-    account = get_account()
-    lottery = Lottery[-1]
-    # fund contract with LINK
-    tx = fund_with_link(lottery.address)
-    tx.wait(1)
-    ending_tx = lottery.endLottery({"from": account})
-    ending_tx.wait(1)
-    time.sleep(60)
-    print_line(f"{lottery.recentWinner()} is the new winner!")
-
+    print_line("Deployed!")
+    return doggieWalkNFTs
 
 def main():
-    deploy_lottery()
-    start_lottery()
-    enter_lottery()
-    end_lottery()
-
-
-    
+    deploy_doggies()
