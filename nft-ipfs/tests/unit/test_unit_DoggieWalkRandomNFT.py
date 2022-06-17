b59bfb3b67ad48bee1463193a98bf28b7750a6a7
Freddie von Stange <freddie71010@gmail.com>
2022-06-17 14:47:08 -0400

    Add unittest and rename AggregatorV3Mock.sol

---
+++ nft-ipfs/tests/unit/test_unit_DoggieWalkRandomNFT.py
@@ -0,0 +1,10 @@
+from brownie import DoggieWalkRandomNFT, network
+import pytest
+from scripts.utils import (
+    get_account,
+    get_contract,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+def test_can_create_doggiewalknfts():
+    pass
