b5269e17a8345286bc20d9ac0e8de3758ecf67b0
Freddie von Stange <freddie71010@gmail.com>
2022-07-22 18:04:39 -0400

    WIP of int and unit tests

--- nft-ipfs/tests/unit/test_unit_DoggieWalkRandomNFT.py
+++ nft-ipfs/tests/unit/test_unit_DoggieWalkRandomNFT.py
@@ -1,10 +1,24 @@
-from brownie import DoggieWalkRandomNFT, network
-import pytest
+from brownie import DoggieWalkNFT, network, config
+import pytest, os
+from scripts import s01__deploy_nft_collection
 from scripts.utils import (
     get_account,
     get_contract,
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
 )
+from web3 import Web3
+from dotenv import load_dotenv
 
-def test_can_create_doggiewalknfts():
-    pass
+load_dotenv()
+
+
+def test_doggie_fee():
+    # Arrange
+    account = get_account()
+    # Act
+    doggie_nft_collectible = s01__deploy_nft_collection.deploy_collection(
+        dogTokenURI_cids_filename=os.getenv("CIDS_TXT_FILE"))
+    expected_doggie_fee = Web3.toWei(0.01, "ether")
+    doggie_fee = doggie_nft_collectible.getMintFee()
+    # Assert
+    assert doggie_fee == expected_doggie_fee
