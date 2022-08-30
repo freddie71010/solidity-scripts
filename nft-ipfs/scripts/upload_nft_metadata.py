5ad91930a4940ccb42493a2ee1f5cafce3569d6a
Freddie von Stange <freddie71010@gmail.com>
2022-08-30 19:20:01 -0400

    upload_nft_metadata script works

--- nft-ipfs/scripts/upload_nft_metadata.py
+++ nft-ipfs/scripts/upload_nft_metadata.py
@@ -19,48 +19,68 @@ class MetadataCollection:
         """
         self.dogTokenURI_cids_filename: str = dogTokenURI_cids_filename
         self.collection_network: str = collection_network 
+        self.metadata_basedir: str = ""
 
-    def create_collection_metadata(self):
+    def create_collection_metadata(self, overwrite: bool = False):
         print(f"Creating metadata JSON files for *{self.collection_network}* network...")
         (doggie_dict, _) = get_dog_cids(self.dogTokenURI_cids_filename, set_collection_size_limit=True)
 
+        # Check local folder and files
         metadata_basedir: Path = Path(f"./metadata/{self.collection_network}/")
+        if Path(metadata_basedir).exists() and not overwrite:
+            raise FileExistsError(f"'{metadata_basedir}' already exists, delete it to continue!")
+        elif Path(metadata_basedir).exists() and overwrite:
+            print(f"'{metadata_basedir}' already exists, wiping folder clean and regenerating data.")
+            print(f"Removing: {len(list(metadata_basedir.iterdir()))} files", end=" ")
+            [file.unlink() for file in metadata_basedir.iterdir()]
+            print("-> All files removed.")
+                
         metadata_basedir.mkdir(parents=True, exist_ok=True)
-
-        
+        self.metadata_basedir = metadata_basedir
 
         for i, doggie in enumerate(doggie_dict.keys(), start=1):
             # num: str = f"0{str(i)}" if i < 10 else f"{str(i)}"
             # metadata_file_name: str = f"{num}_{doggie.split('.')[0]}.json"
             metadata_file_name: str = f"{doggie}.json"
-            metadata_pathway: str = str(metadata_basedir) + "/" + metadata_file_name
-            if Path(metadata_pathway).exists():
-                raise FileExistsError(f"'{metadata_pathway}' already found, delete it to overwrite!")
-            else:
-                print(f"Creating Metadata file: {metadata_file_name}", end="")
-                md = metadata_json = metadata_template.template
+            print(f"Creating Metadata file #{i}: {metadata_file_name}", end="")
+            md = metadata_json = metadata_template.template
+            image_uri = f"ipfs://{doggie_dict[doggie]}"
+            doggie_name = doggie.replace("-", " ").title()
+            
+            md['name'] = doggie_name
+            md['description'] = f"A {doggie_name} dog!"
+            md['image'] = image_uri
+            md['attributes']['trait_type'] = ["cuteness", "happiness", "anger"][len(doggie_name) % 3]
+            md['attributes']['level'] = (len(doggie_name) % 10) + 1
 
-                image_uri = f"ipfs://{doggie_dict[doggie]}"
-                doggie_name = doggie.replace("-", " ").title()
-                
-                md['name'] = doggie_name
-                md['description'] = f"A {doggie_name} dog!"
-                md['image'] = image_uri
-                md['attributes']['trait_type'] = ["cuteness", "happiness", "anger"][len(doggie_name) % 3]
-                md['attributes']['level'] = (len(doggie_name) % 10) + 1
-
-                # Save metadata to JSON file
-                with open(f"{metadata_basedir}/{metadata_file_name}", "w") as f:
-                    json.dump(metadata_json, f, indent=4)
-                
-                print(" -> Done!")
+            # Save metadata to JSON file
+            with open(f"{metadata_basedir}/{metadata_file_name}", "w") as f:
+                json.dump(metadata_json, f, indent=4)
+            
+            print(" -> Done!")
                 
         print("All metadata JSON files saved!")
 
-    def upload_metadata(self):
-        # check 
-        ...
+    def upload_metadata(self, image_pathway: str, collection_name: str, pinata_api_key: str, pinata_api_secret: str):
+            # customize
+            image_pathway = self.metadata_basedir
+            if image_pathway is None or image_pathway == "":
+                raise Exception
 
+            PinataUploader = PinataPy(pinata_api_key, pinata_api_secret, collection_name)
+            resp_pin_files: ResponsePayload = PinataUploader.pin_file_to_ipfs(
+                image_pathway,
+                ipfs_destination_path=collection_name, 
+                save_absolute_paths=False
+            )
+            print(f"Result: {resp_pin_files}")
+
+            if resp_pin_files.get('isDuplicate') == False:
+                resp_pin_list: ResponsePayload = PinataUploader.pin_list({"status": "pinned", "metadata[name]": collection_name})
+                print(f"Pinned List: {resp_pin_list}")
+
+            resp_ipfs_cids: dict = PinataUploader.download_ipfs_file_cids()
+            print(f"IPFS File CIDs: {resp_ipfs_cids}")
 
 
 def main():
@@ -68,5 +88,10 @@ def main():
         dogTokenURI_cids_filename = os.getenv("CIDS_SUMMARY_FILE"),
         # collection_network = 'rinkeby'
     )
-    metadata_collection.create_collection_metadata()
-    metadata_collection.upload_metadata()
+    metadata_collection.create_collection_metadata(overwrite=True)
+    metadata_collection.upload_metadata(
+        image_pathway = None, 
+        collection_name = os.getenv("COLLECTION_NAME") + "_metadata", 
+        pinata_api_key = os.getenv("PINATA_API_KEY"),
+        pinata_api_secret = os.getenv("PINATA_API_SECRET")
+    )
