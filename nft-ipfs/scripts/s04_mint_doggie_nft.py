62c293bbcc4b84f12d3d68e050c736a5ffa4cd30
Freddie von Stange <freddie71010@gmail.com>
2022-09-26 21:39:29 -0400

    Add mint fee as a kwargs for mint_doggie_nft.py

--- nft-ipfs/scripts/s04_mint_doggie_nft.py
+++ nft-ipfs/scripts/s04_mint_doggie_nft.py
@@ -1,6 +1,6 @@
 import os
 
-from brownie import Contract, DoggieWalkNFT
+from brownie import Contract, DoggieWalkNFT, web3 as brownie_web3
 from dotenv import load_dotenv
 
 from .s03_deploy_nft_collection import deploy_collection
@@ -8,12 +8,13 @@ from .utils import get_account, get_name_of_breed, listen_for_event, print_line
 
 load_dotenv()
 
-def mint_doggie_nft(*args):
+def mint_doggie_nft(**kwargs):
     account = get_account(env="MM1")
-    doggie_nft_collection = args[0] if args else DoggieWalkNFT[-1]
+    doggie_nft_collection = kwargs.get('contract', DoggieWalkNFT[-1])
     print_line(f"Doggie Collectible contract address: {doggie_nft_collection.address}")
-    starting_tx = doggie_nft_collection.requestDoggie({"from": account})
-    # starting_tx = doggie_nft_collectible.requestDoggie({"from": account, "amount": 10000000000000000})    # 0.01 eth
+    starting_tx = doggie_nft_collection.requestDoggie(
+        {"from": account, "amount": kwargs.get('fee', brownie_web3.toWei(0.01, "ether")) + 100}
+    )
     starting_tx.wait(1)
 
     print_line("Mint Doggie has started!")
@@ -31,18 +32,19 @@ def mint_doggie_nft(*args):
 
 def main():
     existing_collection = False
-    number_to_mint = 3
+    number_to_mint = 5
+    mint_fee = brownie_web3.toWei(0.02, "ether")
     
     if existing_collection is True:    
         existing_contract = Contract.from_explorer(os.getenv("EXISTING_CONTRACT"))
         print(f"Connected to existing contract: {existing_contract.address}")
         for _ in range(number_to_mint):
-            mint_doggie_nft(existing_contract)
+            mint_doggie_nft(contract=existing_contract, fee=mint_fee)
     else:
         print("Deploying a new contract...")
         deploy_collection(doggiewalk_cids_filename = os.getenv("CIDS_METADATA_FILE"))
         for _ in range(number_to_mint):
-            mint_doggie_nft()
+            mint_doggie_nft(fee=mint_fee)
     print("end")
 
     
